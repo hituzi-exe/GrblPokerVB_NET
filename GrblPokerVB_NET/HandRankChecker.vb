a2d6f75531ab3e425d2d0b5e1898e1fbb01c0cbd
hituzi-exe <sparychory@gmail.com>
2019-01-09 16:32:47 +0900

    modify HandRankChecker

--- GrblPokerVB_NET/HandRankChecker.vb
+++ GrblPokerVB_NET/HandRankChecker.vb
@@ -17,13 +17,51 @@
     End Function
 
 
-    Public Function GetHandRank(hand1, hand2, hand3, hand4, hand5) As Integer
+    Public Function GetHandRank(hand1 As Integer,
+                                hand2 As Integer,
+                                hand3 As Integer,
+                                hand4 As Integer,
+                                hand5 As Integer) As Integer
+        Dim pair = GetRateNumOfAKind(hand1, hand2, hand3, hand4, hand5)
+
+        If pair <> rate.NotPair() Then
+            Return pair
+        End If
+
+        Dim flushFlg As Boolean = IsFlush(hand1, hand2, hand3, hand4, hand5)
+        Dim straightFlg As Boolean = IsStraight(hand1, hand2, hand3, hand4, hand5)
+
+
+
+        If Not (flushFlg Or straightFlg) Then
+            Return rate.HighCard()
+        End If
+
+
+        If flushFlg And straightFlg Then
+            If isRoyalStraightFlush(hand1, hand2, hand3, hand4, hand5) Then
+                Return rate.RoyalStraightFlush()
+
+            End If
+            Return rate.StraightFlush()
+        End If
+
+        If flushFlg Then
+            Return rate.Flush()
+
+        End If
+
+        Return rate.Straight()
 
     End Function
 
-    Public Function GetRateNumOfAKind(hand1, hand2, hand3, hand4, hand5) As Integer
+    Public Function GetRateNumOfAKind(hand1 As Integer,
+                                      hand2 As Integer,
+                                      hand3 As Integer,
+                                      hand4 As Integer,
+                                      hand5 As Integer) As Integer
 
-        If bitCount((hand1 Or hand2 Or hand3 Or hand4 Or hand5) And (&H1FFF)) = 5 Then
+        If BitCount((hand1 Or hand2 Or hand3 Or hand4 Or hand5) And (&H1FFF)) = 5 Then
             Return rate.NotPair()
         End If
 
@@ -34,7 +72,11 @@
     End Function
 
 
-    Public Function PairCount(hand1, hand2, hand3, hand4, hand5) As Tuple(Of Integer, Integer)
+    Public Function PairCount(hand1 As Integer,
+                              hand2 As Integer,
+                              hand3 As Integer,
+                              hand4 As Integer,
+                              hand5 As Integer) As Tuple(Of Integer, Integer)
         Dim cntList = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
         Dim cnt = {hand1 & (&H1FFF),
                    hand2 & (&H1FFF),
@@ -53,7 +95,64 @@
         Return Tuple.Create(cntList.Max() + cntList(13), pairCnt)
     End Function
 
-    Public Function MyLog2(x) As Integer
+    Public Function IsFlush(hand1 As Integer,
+                            hand2 As Integer,
+                            hand3 As Integer,
+                            hand4 As Integer,
+                            hand5 As Integer) As Boolean
+        Return (hand1 And hand2 And hand3 And hand4 And hand5 And (&HF0000)) > 0
+    End Function
+
+    Public Function IsStraight(hand1 As Integer,
+                               hand2 As Integer,
+                               hand3 As Integer,
+                               hand4 As Integer,
+                               hand5 As Integer) As Boolean
+
+        Dim handNum As Integer = (hand1 Or hand2 Or hand3 Or hand4 Or hand5) And (&H1FFF)
+
+        Dim checkbit As Integer = Int(handNum / (handNum And (-handNum)))
+
+        If (checkbit = &H1F) Then
+            Return True
+        End If
+
+        If checkbit = &H1E01 Then
+            Return True
+        End If
+
+        Dim handlist = {hand1, hand2, hand3, hand4, hand5}
+        ' Jokerがなければストレート不成立
+        If Not handlist.ToList().Any(Function(e) e = &HF0000) Then
+            Return False
+        End If
+
+        Dim straightList = {&HF, &H1D, &H1B, &H17, &H1C01, &H1601, &H1A01, &HC01}
+
+        Return straightList.ToList().Any(Function(e) e = checkbit)
+    End Function
+
+
+
+    Public Function isRoyalStraightFlush(hand1 As Integer,
+                                         hand2 As Integer,
+                                         hand3 As Integer,
+                                         hand4 As Integer,
+                                         hand5 As Integer) As Boolean
+
+        Dim handlist = {hand1, hand2, hand3, hand4, hand5}
+        ' Jokerがあれば不成立
+        If handlist.ToList().Any(Function(e) e = &HF0000) Then
+            Return False
+        End If
+
+
+        Return (hand1 Or hand2 Or hand3 Or hand4 Or hand5) And (&H1FFF) = (&H1E01)
+
+    End Function
+
+
+    Public Function MyLog2(x As Integer) As Integer
         If x = 0 Then
             Return 13
         End If

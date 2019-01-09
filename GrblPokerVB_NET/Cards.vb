7ce5a2111f21fb4685e6a01b0f03b5ee44c81812
hituzi-exe <sparychory@gmail.com>
2019-01-09 17:34:24 +0900

    add Cards class

---
+++ GrblPokerVB_NET/Cards.vb
@@ -0,0 +1,68 @@
+﻿Public Class Cards
+    Public Shared JOKER As Integer = &HF0000
+
+    Public Function CreateDeck()
+        ' S = suit bit(sdhc), N = num bit(a23456789tjqk)
+        ' card : chds ---k qjt9 8765 432a
+        Dim num = Enumerable.Range(0, 13).Select(Of Integer)(Function(e) 1 << e)
+        Dim suit = Enumerable.Range(15, 4).Select(Of Integer)(Function(e) 1 << e)
+
+        Dim deck As New ArrayList()
+        For Each n In num
+            For Each s In suit
+                deck.Add(n + s)
+            Next
+        Next
+
+        deck.Add(Cards.JOKER)
+
+        Return deck
+    End Function
+
+    Public Function Convert(hand)
+
+        If hand = "J" Then
+            Return Cards.JOKER
+        End If
+
+        Dim suitsStr = "sdhc"
+        Dim numsStr = "a23456789tjqk"
+
+
+        Dim suit = hand.Substring(0, 1)
+        Dim num = hand.Substring(1, 1)
+
+        Try
+            Return (1 << (suitsStr.IndexOf(suit) + 16)) Or (1 << numsStr.IndexOf(num))
+
+        Catch ex As Exception
+            Return -1
+        End Try
+
+    End Function
+
+    Public Function Convert2(hand)
+
+        If hand = Cards.JOKER Then
+            Return "J"
+        End If
+
+        Dim suits = (hand And &HF0000) >> 16
+        Dim num = hand And &H1FFF
+
+        Dim suitsStr = "sdhc"
+        Dim numsStr = "a23456789tjqk"
+
+        Return GetChar(suitsStr, MyLog2(suits)) & GetChar(numsStr, MyLog2(num))
+
+    End Function
+
+    Public Function MyLog2(x As Integer) As Integer
+        If x = 0 Then
+            Return 13
+        End If
+
+        Return Int(Math.Log(x, 2))
+    End Function
+
+End Class
--- GrblPokerVB_NET/Module1.vb
+++ GrblPokerVB_NET/Module1.vb
@@ -1,7 +1,42 @@
-﻿Module Module1
+﻿Imports System
 
-    Sub Main()
+Module Module1
 
-    End Sub
+    Function Main() As Integer
+        'Dim arguments As String() = Environment.GetCommandLineArgs()
 
+        'Return TestGetHandRank(arguments)
+
+        TestCards()
+
+
+    End Function
+
+    Function TestGetHandRank(arguments As String())
+
+        Dim hrc As New HandRankChecker(New Rate1000())
+        Try
+            Dim res = hrc.GetHandRank(Integer.Parse(arguments(1)),
+                                      Integer.Parse(arguments(2)),
+                                      Integer.Parse(arguments(3)),
+                                      Integer.Parse(arguments(4)),
+                                      Integer.Parse(arguments(5)))
+
+            Console.WriteLine(res)
+
+            Return res
+        Catch ex As System.IndexOutOfRangeException
+            Return -1
+        End Try
+    End Function
+
+
+    Function TestCards() As Integer
+        Dim c As New Cards()
+
+        c.CreateDeck()
+
+        Return 0
+
+    End Function
 End Module

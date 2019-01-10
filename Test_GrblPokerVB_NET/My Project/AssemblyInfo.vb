b0a8ea18beb484dcbd0d35da82ea5e9c9bf5d4e7
hituzi-exe <sparychory@gmail.com>
2019-01-10 10:00:04 +0900

    add unit test project

---
+++ Test_GrblPokerVB_NET/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("Test_GrblPokerVB_NET")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Test_GrblPokerVB_NET")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("ea21fd17-fd40-4446-9d5d-9d0b5417c762")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Test_GrblPokerVB_NET/TestHandRankChecker.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports GrblPokerVB_NET
+
+<TestClass()> Public Class TestHandRankChecker
+
+    <TestMethod()> Public Sub TestBitCount()
+        Dim r As New Rate1000()
+        Dim c As New HandRankChecker(r)
+
+        Dim res = c.BitCount(&B11100111)
+
+        Assert.AreEqual(res, 6)
+
+    End Sub
+
+    <TestMethod()> Public Sub TestIsFlush()
+        Dim card As New Cards()
+        Dim r As New Rate1000()
+        Dim c As New HandRankChecker(r)
+
+        Dim c1 = card.Convert("J")
+        Dim c2 = card.Convert("s1")
+        Dim c3 = card.Convert("s7")
+        Dim c4 = card.Convert("s9")
+        Dim c5 = card.Convert("st")
+
+        Dim res = c.IsFlush(c1, c2, c3, c4, c5)
+
+        Assert.IsTrue(res)
+
+    End Sub
+
+End Class

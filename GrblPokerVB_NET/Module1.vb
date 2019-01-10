26ab7826209b759d7cc0673dec9725fcc56b832f
hituzi-exe <sparychory@gmail.com>
2019-01-10 14:06:45 +0900

    add GetMaxExpectation

--- GrblPokerVB_NET/Module1.vb
+++ GrblPokerVB_NET/Module1.vb
@@ -3,41 +3,10 @@
 Module Module1
 
     Function Main() As Integer
-        'Dim arguments As String() = Environment.GetCommandLineArgs()
+        Return GetMaxExpectation("s1", "s3", "h6", "h8", "dt")
+        'Dim args As String() = Environment.GetCommandLineArgs()
 
-        'Return TestGetHandRank(arguments)
-
-        TestCards()
-
-
-    End Function
-
-    Function TestGetHandRank(arguments As String())
-
-        Dim hrc As New HandRankChecker(New Rate1000())
-        Try
-            Dim res = hrc.GetHandRank(Integer.Parse(arguments(1)),
-                                      Integer.Parse(arguments(2)),
-                                      Integer.Parse(arguments(3)),
-                                      Integer.Parse(arguments(4)),
-                                      Integer.Parse(arguments(5)))
-
-            Console.WriteLine(res)
-
-            Return res
-        Catch ex As System.IndexOutOfRangeException
-            Return -1
-        End Try
-    End Function
-
-
-    Function TestCards() As Integer
-        Dim c As New Cards()
-
-        c.CreateDeck()
-
-        Return 0
-
+        'Return GetMaxExpectation(args(1), args(2), args(3), args(4), args(5))
     End Function
 
     Function GetMaxExpectation(hand1 As String,
@@ -46,20 +15,76 @@ Module Module1
                                hand4 As String,
                                hand5 As String) As Integer
 
-        Dim c As New Cards()
-        Dim deck = c.CreateDeck()
+        Dim card As New Cards()
+        Dim deck = card.CreateDeck()
         Dim hands = {hand1, hand2, hand3, hand4, hand5}.
             ToList().
-            Select(Of Integer)(Function(e) c.Convert(e))
-
+            Select(Of Integer)(Function(e) card.Convert(e))
 
         deck.RemoveAll(AddressOf hands.Contains)
 
+        Dim checker As New HandRankChecker(New Rate1000)
+
+        Dim maxExp As Double = 0
+        Dim maxExpHand As New List(Of Integer)
+
+        For Each i In {0, 1, 2, 3, 4, 5}
+            For Each hand In Combination(Of Integer)(hands.ToList, i)
+                Dim sumCount = 0
+                Dim sumExp = 0
+
+                For Each d In Combination(Of Integer)(deck, 5 - i)
+                    Dim c As Integer() = New Integer(hand.Count + d.Count - 1) {}
+                    Array.Copy(hand.ToArray, c, hand.Count)
+                    Array.Copy(d.ToArray, 0, c, hand.Count, d.Count)
+
+                    sumExp += checker.getHandRank(c(0), c(1), c(2), c(3), c(4))
+                    sumCount += 1
+
+                Next
+
+                If maxExp < (sumExp / sumCount) Then
+                    maxExp = (sumExp / sumCount)
+                    maxExpHand = hand
+                End If
+
+            Next
+        Next
 
 
-
+        For Each h In maxExpHand
+            Console.Write(card.Convert2(h) & ",")
+        Next
 
     End Function
+
+    Function Combination(Of T)(elements As List(Of T), choose As Integer) As List(Of List(Of T))
+        Dim ret As New List(Of List(Of T))
+
+        ' 再帰呼び出しの終端
+        If elements.Count < choose Then
+            Return New List(Of List(Of T))()
+        ElseIf choose <= 0 Then
+            Dim resList As New List(Of List(Of T)) From {
+                New List(Of T)
+            }
+            Return resList
+        End If
+
+        For n As Integer = 1 To elements.Count - 1
+            Dim subRet = Combination(elements.Skip(n).ToList(), choose - 1)
+
+            For Each s In subRet
+                s.Add(elements(n - 1))
+
+            Next
+            ret.AddRange(subRet)
+        Next
+        Return ret
+    End Function
+
+
+
 
 
 End Module
